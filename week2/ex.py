36d218223ce30c4c6a1ab971fc4fcefe3099265f
jamestang7 <james.tangsw@gmail.com>
2022-10-18 08:48:24 -0400

    finish lecture 7

---
+++ week2/ex.py
@@ -0,0 +1,108 @@
+from math import remainder
+
+
+
+
+def apply_twice(f,x):
+    return f(f(x))
+def square(x):
+    return x * x
+
+def repeat(f, x):
+    while f(x) != x:
+        x = f(x)
+    return x 
+def g(y):
+    return (y + 5) // 3
+
+# def make_adder(n):
+#     def adder(k):
+#         return k + n 
+#     return adder 
+
+def make_add(n):
+    return lambda x: x + n 
+
+def f(x, y):
+    return g(x)
+def g(a):
+    return a + y 
+# not nested 'y', y not found in the environment 
+
+def triple(x):
+    return 3 * x 
+
+def compose1(f, g):
+    def h(x):
+        return f(g(x))
+    return h 
+
+def print_all(x):
+    print(x)
+    return print_all
+
+def print_sum(x):
+    print(x)
+    def next_sum(y):
+        return print_sum(x+y)
+    return next_sum
+
+def curry2(f):
+    def g(x):
+        def h(y):
+            return f(x,y)
+        return h 
+    return g 
+
+def likes(n):
+    return n % 2 == 0
+def mystery2(n):
+    i, j, k = 0, None, None
+    while i < n:
+        if likes(i):
+            if j != None and (k == None or i - j < k):
+                k = i - j 
+            j = i
+        i += 1
+    return k 
+# mystery2(8)
+
+def remove(n, k):
+    """
+    Return digits in n that are not k 
+    >>> remove(231, 3)
+    21
+    >>> remove(251512,5)
+    2112
+    """
+    kept, digits = 0, 0 
+    while n % 10 != 0:
+        n, remainder = n // 10, n % 10
+        if remainder != k:
+            kept += (10 ** digits) * remainder 
+        else: digits -= 1
+        digits += 1 
+    return kept 
+
+# remove(251512,5)
+
+def trace(fn):
+    def wrapped(x):
+        print('->', fn, '(', x, ')')
+        return fn(x)
+    return wrapped 
+@trace 
+def square(x):
+    return x ** 2
+## whenever call square, it turns out to be
+## square = trace(square)
+## i.e. square(5) = trace(sqaure(5))
+
+def sum_squares_up_to(n):
+    k = 1 
+    total = 0
+    while k <= n:
+        total, k = total + square(k), k + 1
+    return total 
+
+
